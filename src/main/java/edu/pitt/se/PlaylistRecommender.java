eb37ae2b88d628ca4a1a2fc73ff78a30f4cb0a30
Marcos <mfc56@pitt.edu>
2025-12-01 19:55:51 -0500

    Update PlaylistRecommender.java

--- src/main/java/edu/pitt/se/PlaylistRecommender.java
+++ src/main/java/edu/pitt/se/PlaylistRecommender.java
@@ -5,17 +5,54 @@ import java.util.List;
 public class PlaylistRecommender {
 
     public static String classifyEnergy(List<Integer> bpms) {
-        // TODO: Implement classifyEnergy()
-        throw new UnsupportedOperationException("Not implemented");
+        //Reject bad cases
+        if(bpms == null){
+            return "ERROR! List cannot be NULL";
+        }
+        else if(bpms.size() == 0){
+            return "ERROR! List cannot be empty";
+        }
+        //Calculate average
+        int total = 0;
+        for(int i = 0; i <= bpms.size(); i++){
+            total += bpms.get(i);
+        }
+        int average = total/bpms.size();
+        //Diffrentiate outcomes
+        if(average >= 140){
+            return "HIGH";
+        }
+        else if(average >= 100){
+            return "MEDIUM";
+        }
+        else{
+            return "LOW";
+        }
     }
 
     public static boolean isValidTrackTitle(String title) {
-        // TODO: Implement isValidTrackTitle()
-        throw new UnsupportedOperationException("Not implemented");
+        //reject bad cases
+        if(title == null){
+            return false;
+        }
+        else if(title.length() == 0){
+            return false;
+        }
+        else if(title.length() > 30){
+            return false;
+        }
+        //ensure the title only contains letters and spaces
+        char[] charArray = title.toCharArray();
+        for(int i = 0; i <= title.length(); i++){
+            if(!Character.isLetter(charArray[i]) && (charArray[i] != ' ')){
+                return false;
+            }
+        }
+        return true;
     }
 
     public static int normalizeVolume(int volumeDb) {
-        // TODO: Implement normalizeVolume()
-        throw new UnsupportedOperationException("Not implemented");
+        int clamp = volumeDb + 10;
+        return clamp * 100 / 130;
     }
 }
